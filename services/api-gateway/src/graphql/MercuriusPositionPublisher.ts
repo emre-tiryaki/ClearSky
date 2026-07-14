5d4b652e37531906ec9fadfdbc45eac6b223c711
Emre Tiryaki <tiryakiemre18@gmail.com>
2026-07-14 14:01:47 +0300

    feat(api-gateway): add MercuriusPositionPublisher class for publishing messages

---
+++ services/api-gateway/src/graphql/MercuriusPositionPublisher.ts
@@ -0,0 +1,11 @@
+import type { PubSub } from "mercurius";
+import type { PositionPublisher } from "./PositionPublisher.js";
+
+export class MercuriusPositionPublisher implements PositionPublisher {
+    constructor(private readonly pubsub: PubSub) {}
+
+    async publish(topic: string, payload: unknown): Promise<void> {
+        await this.pubsub.publish({topic, payload});
+    }
+
+}
