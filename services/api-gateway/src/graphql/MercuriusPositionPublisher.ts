6574d5d45d37b5a3eb9fb9bb7add0ee66bed9a27
Emre Tiryaki <tiryakiemre18@gmail.com>
2026-07-16 12:37:45 +0300

    chore(api-gateway, device-interface): removed the logs that i put for testing from both services

--- services/api-gateway/src/graphql/MercuriusPositionPublisher.ts
+++ services/api-gateway/src/graphql/MercuriusPositionPublisher.ts
@@ -5,11 +5,7 @@ export class MercuriusPositionPublisher implements PositionPublisher {
     constructor(private readonly pubsub: PubSub) { }
 
     async publish(topic: string, payload: unknown): Promise<void> {
-        // THIS CODE IS JUST FOR TESTING AND DOES NOT AFFECT ANY WORK IN THIS SERVICE.
-        // WILL PROBABLY BE DELETED WHEN MERGING.
-        console.log(`${topic} is publishing`);
         await this.pubsub.publish({ topic, payload });
-        console.log(`${topic} is published`);
     }
 
 }
