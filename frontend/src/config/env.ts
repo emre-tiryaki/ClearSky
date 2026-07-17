9748ac3fb796ff3f231b459ab9e2437c0c020bc6
Emre Tiryaki <tiryakiemre18@gmail.com>
2026-07-17 12:44:48 +0300

    feat(frontend): added env.ts for loading .env config easily. Also added "@types/node" as a dev dependency

---
+++ frontend/src/config/env.ts
@@ -0,0 +1,17 @@
+export interface FrontendConfig {
+    VITE_GRAPHQL_WS_URL: string;
+}
+
+function requireEnv(name: string): string {
+    const value = process.env[name];
+    if (!value)
+        throw new Error(`required field is missing in .env: ${name}`);
+
+    return value;
+}
+
+function loadConfig(): FrontendConfig {
+    return {
+        VITE_GRAPHQL_WS_URL: requireEnv('VITE_GRAPHQL_WS_URL'),
+    }
+}
