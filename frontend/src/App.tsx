7683545de15b00729c4b8275f1a47fa72ccb8b44
Emre Tiryaki <tiryakiemre18@gmail.com>
2026-07-20 10:49:50 +0300

    feat(frontend): successfullt showed map in frontend does not work tho, will fix it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,29 @@
+import { useMemo } from "react"
+import type { BoundingBox } from "./types/fligt"
+import { useLiveFlights } from "./hooks/useLiveFlights";
+import { FlightMap } from "./components/FlightMap";
+
+const TURKEY_BBOX: BoundingBox = {
+  lamin: 34.0,
+  lomin: 24.0,
+  lamax: 43.5,
+  lomax: 43.5
+}
 
 function App() {
+  const bbox = useMemo(() => TURKEY_BBOX, []);
+  const flights = useLiveFlights(bbox);
 
   return (
-    <h1>hello world</h1>
+    <div className="h-screen w-screen flex flex-col">
+      <header className="bg-slate-900 text-white px-4 py-2 flex items-center justify-between">
+        <h1 className="text-lg font-semibold">Clear Sky - Live Flight Dashboard</h1>
+        <span className="text-sm text-slate-300">{flights.size} planes are showing</span>
+      </header>
+      <main className="flex-1">
+        <FlightMap flights={flights}/>
+      </main>
+    </div>
   )
 }
 
