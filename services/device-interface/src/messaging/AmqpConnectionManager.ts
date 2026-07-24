f128bcdf5d6c351e6a16edd51a5aac7381f69631
Emre Tiryaki <tiryakiemre18@gmail.com>
2026-07-24 13:47:41 +0300

    feat(device-interface): configured AmqpConnectionManager so it does not fail when channel is closed, instead it tries to reconnect.

--- services/device-interface/src/messaging/AmqpConnectionManager.ts
+++ services/device-interface/src/messaging/AmqpConnectionManager.ts
@@ -1,9 +1,11 @@
 import amqplib, { type Channel } from "amqplib";
+import { resolve } from "node:dns";
 
 // Manages the AMQP connection and channel lifecycle for the service.
 export class AmqpConnectionManager {
     private connection: amqplib.ChannelModel | null = null;
     private channel: Channel | null = null;
+    private reconnecting: boolean = false;
 
     constructor(
         private readonly url: string,
@@ -16,18 +18,49 @@ export class AmqpConnectionManager {
         this.connection = await amqplib.connect(this.url);
         this.channel = await this.connection.createChannel();
         await this.assertExchange();
-    }
 
+        this.connection.on('error', err => {
+            console.error('AmqpConnectionManager: connection error', err);
+            void this.scheduleReconnect();
+        })
+
+        this.connection.on('close', () => {
+            console.error('AmqpConnectionManager: connection closed...');
+            void this.scheduleReconnect();
+        })
+    }
+    
     // Returns the active AMQP channel after connect() has been called.
     getChannel(): Channel {
         if (!this.channel)
             throw new Error("AmqbConnectionManager: cant use getChannel before connect()");
-
+        
         return this.channel;
     }
-
+    
     // Declares the exchange used for publishing messages.
     async assertExchange(): Promise<void> {
         await this.getChannel().assertExchange(this.exchangeName, this.exchangeType, {durable: true});
+    }
+
+    private async scheduleReconnect(): Promise<void> {
+        if (this.reconnecting) return;
+
+        this.reconnecting = true;
+        this.channel = null;
+
+        while(true) {
+            try {
+                await new Promise(resolve => setTimeout(resolve, 5000));
+                console.log(`AmqpConnectionManager: attempting to reconnect...`);
+                await this.connect();
+                console.log('AmqpConnectionManager: reconnected succesfully');
+                this.reconnecting = false;
+                return;
+            } catch (err) {
+                const message = err instanceof Error ? err.message : String(err);
+                console.error(`AmqpConnectionManager: reconnect failed, retrying in 5 s... (${message})`);
+            }
+        }
     }
 }
