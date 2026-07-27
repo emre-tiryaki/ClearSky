ab634b6837cd602cc6c11314e70605e99d4f5c7c
Emre Tiryaki <tiryakiemre18@gmail.com>
2026-07-27 12:51:24 +0300

    feat(api-gateway): reconfigured FlightRepository class for getting flights from database by time and icao24 id, also added indexes to icao24 and recordedAt fields of the collection.

--- services/api-gateway/src/persistence/FlightRepository.ts
+++ services/api-gateway/src/persistence/FlightRepository.ts
@@ -8,6 +8,7 @@ export class FlightRepository {
 
     constructor(db: Db) {
         this.collection = db.collection<FlightRecordDocument>(COLLECTION_NAME);
+        void this.collection.createIndex({ icao24: 1, recordedAt: 1 });
     }
 
     async save(position: FlightPosition, note?: string): Promise<SavedFlightRecord> {
@@ -30,5 +31,23 @@ export class FlightRepository {
 
         const result = await this.collection.insertOne(document);
         return { ...document, _id: result.insertedId.toString() };
+    }
+
+    async findByDateRange(start: Date, end: Date): Promise<SavedFlightRecord[]> {
+        const docs = await this.collection
+            .find({ recordedAt: { $gte: start, $lte: end } })
+            .sort({ recordedAt: 1 })
+            .toArray();
+
+        return docs.map(doc => ({ ...doc, _id: doc._id.toString() }));
+    }
+
+    async findByIcao24(icao24: string): Promise<SavedFlightRecord[]> {
+        const docs = await this.collection
+            .find({ icao24 })
+            .sort({ recordedAt: 1 })
+            .toArray();
+
+        return docs.map(doc => ({ ...doc, _id: doc._id.toString() }));
     }
 }
